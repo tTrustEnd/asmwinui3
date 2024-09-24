d790824e9accaff9e257e9cc3ae8e18819a8c941
Truong <truong.nq185728@gmail.com>
2024-09-24 07:29:22 +0700

    2

--- obj/x86/Debug/net8.0/AsignmentWinUI.Core.AssemblyInfo.cs
+++ obj/x86/Debug/net8.0/AsignmentWinUI.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsignmentWinUI.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55104adf92a5896a96c1860a0e522f48e707514")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsignmentWinUI.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsignmentWinUI.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
