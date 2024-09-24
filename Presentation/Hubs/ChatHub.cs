e55104adf92a5896a96c1860a0e522f48e707514
Truong <truong.nq185728@gmail.com>
2024-09-24 07:27:10 +0700

    1

---
+++ Presentation/Hubs/ChatHub.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+using System.Diagnostics;
+using AsignmentWinUI.Core.Entities;
+using AsignmentWinUI.Core.UseCases.GetOnlineGroupMemberUseCase;
+using AsignmentWinUI.Core.UseCases.Services;
+
+namespace CleanArchitectureSignalR.Presentation.Hubs;
+
+public class ChatHub : Hub
+{
+    private readonly IMessageService _messageService;
+    private readonly IGetOnlineGroupMemberUseCase _getOnlineGroupMemberUseCase;
+    public ChatHub(
+        IMessageService messageService,
+        IGetOnlineGroupMemberUseCase getOnlineGroupMemberUseCase
+        )
+    {
+        _messageService = messageService;
+        _getOnlineGroupMemberUseCase = getOnlineGroupMemberUseCase;
+    }
+    public async Task GetOnlineGroupMembers(int groupId)
+    {
+        try
+        {
+            var member = await _getOnlineGroupMemberUseCase.ExecuteAsync(groupId);
+            foreach (var mb in member)
+            {
+                Debug.WriteLine(
+                    "NAME: " + mb.User?.UserName
+                    );
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            throw;
+        }
+    }
+    public async Task SendMessage(string user, string message)
+    {
+        try
+        {
+            await _messageService.SendMessageAsync(user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            throw; // Propagate the exception to client
+        }
+    }
+    public async Task<IEnumerable<Message>> GetMessages()
+    {
+        try
+        {
+            var Mes = await _messageService.GetMessagesAsync();
+            foreach (var m in Mes)
+            {
+                Debug.WriteLine(m.MessageID + " "
+                    + m.User?.UserName + " "
+                    + m.Group?.GroupName + " "
+                    + m.Content);
+            }
+            return Mes;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureSignalR.Presentation.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsignmentWinUI.Core;
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSignalR();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHub<ChatHub>("/chatHub");
+        });
+
+        // In ra thông báo sau khi cấu hình xong
+        Debug.WriteLine("Server is running.");
+    }
+}
+
+
+
